a893af1ecfdbb0819c8a72fcd3421b61a8e3b9d0
lapinell <lapinell@gmail.com>
2018-02-07 17:15:55 -0600

    added buggy calculator

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# FEDevTutor
-This is where I create exercises for tutoring
+# Buggy Calculator Exercise
+
+The client has called you and is having the following problems:
+1. The CSS is not working, why?
+
---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log("Hello main.js");
+
+function takeValue(x) {
+	document.getElementById('inputwindow').value += x;
+}
+
+function clearInput(y) {
+	document.getElementById('inputwindow').value = y;
+}
+
+function calculateResult() {
+	var result = eval(document.getElementById('inputwindow').value);
+	document.getElementById('inputwindow').value = result;
+}
